df9e2e63d5bd654827ea5beaae0e0beb133c258b
Canos13 <canomsergio13@gmail.com>
2022-07-19 18:46:43 -0500

    page service

---
+++ pages/service.js
@@ -0,0 +1,14 @@
+import Container from '../components/Container'
+
+const Service = () => {
+  return (
+    <>  
+      <Container title="Service">
+          <h2>Service</h2>
+          <p> loremLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. </p>
+      </Container>
+    </>
+  )
+}
+
+export default Service
