31085c01aaa8b2cfedde22f284a2bb46d3c67c21
Canos13 <canomsergio13@gmail.com>
2022-07-19 18:48:00 -0500

    Page about

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import Container from '../components/Container'
+
+const About = () => {
+  return (
+    <>
+      <Container title="About">
+          <h2>About</h2>
+      </Container>
+    </>
+  )
+}
+
+export default About
